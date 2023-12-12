e79405e8f40755b18c88ee3b2df0ad248ed94529
babysowmya <147791943+babysowmya@users.noreply.github.com>
2023-12-12 21:06:03 +0530

    #include<stdio.h> int main() {     int n,i,fact=1;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         if(n%i==0)         {          printf("%d ",i);         }     } }

--- Find_Factors.c
+++ Find_Factors.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int i,n;
+    int n,i,fact=1;
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
         if(n%i==0)
-        printf("%d ",i);
+        {
+         printf("%d ",i);
+        }
     }
 }
