ab6886f0f49a6da43ea26a986c7841aa9809ed7f
babysowmya <147791943+babysowmya@users.noreply.github.com>
2024-03-05 13:04:10 +0530

    Add files via upload

---
+++ insertion_sort.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+void p_a(int *a,int size)
+{
+	for(int i=0;i<size;i++)
+	{
+		printf("%d  ",a[i]);
+	}
+	printf("\n");
+}
+void i_s(int *a,int n)
+{
+	for(int i=1;i<n;i++)
+	{
+		int key=a[i],j=i-1;
+		while(j>=0 && a[j]>key)
+		{
+			a[j+1]=a[j];
+			j--;
+		}
+		a[j+1]=key;
+		
+	}
+}
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	int i,a[n];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	printf("before swapping: ");
+	p_a(a,n);
+	printf("after swapping: ");
+	i_s(a,n);
+	p_a(a,n);
+	return 0;
+}
