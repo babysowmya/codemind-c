00bb58d6dec7201ad2a6e7e8f083b8e5072e0608
babysowmya <147791943+babysowmya@users.noreply.github.com>
2024-03-08 17:10:41 +0530

    Add files via upload

---
+++ biggest.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a%9==0)
+    {
+        printf("a is divisible");
+    }
+    else
+    {
+        printf("a is not divisible");
+    }
+}
