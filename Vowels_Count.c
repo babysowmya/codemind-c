87b9e941be3e8dcede0b08acb2688298bd2dfa4d
babysowmya <147791943+babysowmya@users.noreply.github.com>
2024-03-12 19:34:38 +0530

    #include<stdio.h> int main() {     char str[1001],c=0;     scanf("%[^ ]s",str);     for(int i=0;str[i]!=NULL;i++)     {         if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U')         {             c=c+1;         }     }     printf("%d",c); }

---
+++ Vowels_Count.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char str[1001],c=0;
+    scanf("%[^
+]s",str);
+    for(int i=0;str[i]!=NULL;i++)
+    {
+        if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U')
+        {
+            c=c+1;
+        }
+    }
+    printf("%d",c);
+}
