7af961df0ea520101b57a3c543a0f96f1aa1b996
babysowmya <147791943+babysowmya@users.noreply.github.com>
2024-03-14 19:39:21 +0530

    Add files via upload

---
+++ months.cpp
@@ -0,0 +1,58 @@
+#include<stdio.h>
+int main()
+{
+	int m;
+	scanf("%d",&m);
+	if(m==1)
+	{
+		printf("january");
+	}
+	else if(m==2)
+	{
+		printf("february");
+	}
+	else if(m==3)
+	{
+		printf("march");
+	}
+	else if(m==4)
+	{
+		printf("april");
+	}
+	else if(m==5)
+	{
+		printf("may");
+	}
+	else if(m==6)
+	{
+		printf("june");
+	}
+	else if(m==7)
+	{
+		printf("july");
+	}
+	else if(m==8)
+	{
+		printf("august");
+	}
+	else if(m==9)
+	{
+		printf("septemder");
+	}
+	else if(m==10)
+	{
+		printf("october");
+	}
+	else if(m==11)
+	{
+		printf("november");
+	}
+	else if(m==12)
+	{
+		printf("december");
+	}
+	else
+	{
+		printf("january-december");
+	}
+}
