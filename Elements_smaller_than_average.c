7ad76886fcd626a7ab5309f01db172795227b557
babysowmya <147791943+babysowmya@users.noreply.github.com>
2024-03-19 15:03:51 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int i,a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int sum=0,avg,c=0;     for(i=0;i<n;i++)     {         sum=sum+a[i];     }     avg=sum/n;     for(i=0;i<n;i++)     {         if(a[i]<=avg)         {             c=c+1;         }     }     printf("%d",c); }

---
+++ Elements_smaller_than_average.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int i,a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int sum=0,avg,c=0;
+    for(i=0;i<n;i++)
+    {
+        sum=sum+a[i];
+    }
+    avg=sum/n;
+    for(i=0;i<n;i++)
+    {
+        if(a[i]<=avg)
+        {
+            c=c+1;
+        }
+    }
+    printf("%d",c);
+}
