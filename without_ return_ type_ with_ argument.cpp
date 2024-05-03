e793f0eea5131d3b5b89e4c7734b8428fe6024e3
babysowmya <147791943+babysowmya@users.noreply.github.com>
2024-05-03 19:48:55 +0530

    Add files via upload

---
+++ without_ return_ type_ with_ argument.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int r_n(int n);
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	int res=r_n(n);
+	printf("%d",res);
+}
+int r_n(int a)
+{
+	int r,rev=0;
+	while(a>0)
+	{
+		r=a%10;
+		rev=rev*10+r;
+		a=a/10;
+	}
+	return rev;
+}
