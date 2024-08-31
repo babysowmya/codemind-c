14956b111e72b178ec4356c7b179945d790ee8fe
babysowmya <147791943+babysowmya@users.noreply.github.com>
2024-08-31 09:23:19 +0530

    Create Can_chef_practice_coding.c

---
+++ Can_chef_practice_coding.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+int n;
+scanf("%d",&n);
+while(n--)
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    if(15*a >= 2*b) printf("YES\n");
+    else printf("NO\n");
+    
+}
+}
+
